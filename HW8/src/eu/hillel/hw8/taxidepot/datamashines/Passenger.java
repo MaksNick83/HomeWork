dc845bee8d0eaafbb56e425a32f9982fae3404e3
maxnick83 <maxnick83@ukr.net>
2022-07-29 13:07:15 +0300

    HW8

--- HW8/src/eu/hillel/hw8/taxidepot/datamashines/Passenger.java
+++ HW8/src/eu/hillel/hw8/taxidepot/datamashines/Passenger.java
@@ -3,12 +3,10 @@ package eu.hillel.hw8.taxidepot.datamashines;
 
 public class Passenger extends MotorTransport {
     private int seatsNumber;
-
     public Passenger(int cost, int consumption, int speed, int seatsNumber) {
         super(cost, consumption, speed);
         this.seatsNumber = seatsNumber;
     }
-
 
     @Override
     public String toString() {
